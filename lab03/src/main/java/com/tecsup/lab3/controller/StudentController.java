a2dc6bf2263fddb9a31d29c8bdd42d8b4b2d2161
Alicia Yachi <alicia.yachi@tecsup.edu.pe>
2025-09-01 17:19:03 -0500

    fix: resuelve conflicto en StudentController

--- lab03/src/main/java/com/tecsup/lab3/controller/StudentController.java
+++ lab03/src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -11,7 +11,8 @@ public class StudentController {
     public static void main(String[] args) {
         List<Student> students = Arrays.asList(
                 new Student(1L, "María", "maria@tecsup.edu"),
-                new Student(2L, "Pedro", "pedro@tecsup.edu")
+                new Student(2L, "Pedro", "pedro@tecsup.edu"),
+                new Student(3L, "Lucía", "lucia@tecsup.edu")
         );
 
         StudentReport.print(students);
